b767470c908667664a4041ac31013959f3cd925f
goranshoan <86728496+goranshoan@users.noreply.github.com>
2024-04-28 07:50:46 +0200

    fixed error in dataframe code

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,4 @@ if ingrediensts_list:  # Corrected the typo here
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width_true)
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
