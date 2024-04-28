d9346b5cdae84aef64086481cd842c7a06f15a84
goranshoan <86728496+goranshoan@users.noreply.github.com>
2024-04-28 08:03:43 +0200

    rearranged some lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title("Customize your smothie :tea:")
@@ -24,10 +25,12 @@ ingrediensts_list = st.multiselect(
 )
 
 if ingrediensts_list:  # Corrected the typo here
-    ingredients_string = ''
+        ingredients_string = ''
 
-    for fruit_chosen in ingrediensts_list:  # And here
-        ingredients_string += fruit_chosen + ' '
+    for fruit_chosen in ingrediensts_list: 
+            ingredients_string += fruit_chosen + ' '
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+            fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     st.write(ingredients_string)
 
@@ -42,8 +45,3 @@ if ingrediensts_list:  # Corrected the typo here
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
     st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
-
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
